c98e65b77c567997f0d56665c964709e93e4c7fb
Hello-Own-World <46568151+Hello-Own-World@users.noreply.github.com>
2020-06-29 16:21:48 +0300

    Add files via upload
    The last commit was wrong

--- Application.py
+++ Application.py
@@ -6,22 +6,23 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import *
 
+
 class App(QMainWindow):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         super(App, self).__init__()
-        self.args = args
-        self.kwargs = kwargs
+
+        self.path_to_icon = "C:\\Users\\yushc\\PycharmProjects\\works\\PyQt5projects\\WallpaperApp\\circle.png"
+        self.title = "WallpaperApp"
+
         self.threadpool = QThreadPool()
         print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
 
         uic.loadUi("C:\\Users\\yushc\\PycharmProjects\\works\\PyQt5projects\\WallpaperApp\\untitled.ui", self)
 
         self.button1 = self.findChild(QtWidgets.QPushButton, "pushButton_3")
-        self.button1.clicked.connect(Engine.mainloop)
+        self.button1.clicked.connect(self.mainloop)
 
-        self.path_to_icon = "C:\\Users\\yushc\\PycharmProjects\\works\\PyQt5projects\\WallpaperApp\\circle.png"
-        self.title = "WallpaperApp"
         self.init_ui()
         self.show()
 
@@ -29,52 +30,49 @@ class App(QMainWindow):
         self.setWindowTitle(self.title)
         self.setWindowIcon(QIcon(self.path_to_icon))
 
+    def mainloop(self):
+        worker = Worker()
+        self.threadpool.start(worker)
 
-class Engine(QRunnable):
 
-    def __init__(self, *args, **kwargs):
-        super(Engine, self).__init__()
-        self.args = args
-        self.kwargs = kwargs
+class Worker(QRunnable):            #Окремий потік
 
+    def __init__(self):
+        super(Worker, self).__init__()
         """Default settings"""
         self.SPI_SETDESKWALLPAPER = 20
         self.ph_amount = 6
         self.time_period3 = [0, 7, 9, 14, 18, 20, 24]
+
         """Path to img"""
         self.path = ["C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\1.png",
-                "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\  2\\2.png",
-                "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\3.png",
-                "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\4.png",
-                "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\5.png",
-                "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\6.png"]
-
-    def print_(self):
-        print('1')
+                     "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\  2\\2.png",
+                     "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\3.png",
+                     "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\4.png",
+                     "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\5.png",
+                     "C:\\Users\\yushc\\OneDrive\\Робочий стіл\\photo\\2\\6.png"]
 
     def change_BG(self, i):
         ctypes.windll.user32.SystemParametersInfoW(self.SPI_SETDESKWALLPAPER, 0, self.path[i], 3)
 
-    def mainloop(self):
-        worker = Engine()
-        self.threadpool.start(worker)
-
+    def run(self):
         '''
+        Your code goes in this function
+        '''
+        print("Thread start")
         while True:
             hour = time.localtime()
             for i in range(0, self.ph_amount):
                 if self.time_period3[i] <= hour[3] < self.time_period3[i + 1]:
                     self.change_BG(i)
                     print(hour, i)  # Marker
-                    time.sleep(((self.time_period3[i + 1] - hour[3]) * 60 - hour[4]) * 60)  # wait to next turn
-'''
+                    time.sleep(((self.time_period3[i + 1] - hour[3]) * 60 - hour[4]) * 60)
+
 
 def main():
     app = QApplication(sys.argv)
     window = App()
-    eng = Engine()
     sys.exit(app.exec_())
-
 
 
 if __name__ == '__main__':
